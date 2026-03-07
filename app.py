85f223bed343d16d21f1598568ef2d060a24ba21
Addagiri Mani Raghava Sri <157789269+Mani-Raghava-Sri-Addagiri@users.noreply.github.com>
2026-03-07 20:19:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import threading
 import pyautogui
 
 class API:
+
     def start_gesture(self):
         print("gesture")
         threading.Thread(target=gesture.run).start()
@@ -13,20 +14,58 @@ class API:
         print("volume")
         threading.Thread(target=voice.main).start()
 
+    def start_both(self):
+        print("gesture + voice")
+        threading.Thread(target=gesture.run).start()
+        threading.Thread(target=voice.main).start()
+
     def open_help(self):
         screen_w, screen_h = pyautogui.size()
-        webview.create_window("Help", "ui/help.html", width=screen_w, height=screen_h)
+        webview.create_window(
+            "Help",
+            "ui/help.html",
+            js_api=self,      # IMPORTANT
+            width=screen_w,
+            height=screen_h
+        )
+
+    def open_gesture_help(self):
+        screen_w, screen_h = pyautogui.size()
+        webview.create_window(
+            "Gesture Help",
+            "ui/help_gesture.html",
+            js_api=self,      # IMPORTANT
+            width=screen_w,
+            height=screen_h
+        )
+
+    def open_voice_help(self):
+        screen_w, screen_h = pyautogui.size()
+        webview.create_window(
+            "Voice Help",
+            "ui/help_voice.html",
+            js_api=self,      # IMPORTANT
+            width=screen_w,
+            height=screen_h
+        )
+
+    def close_window(self):
+        webview.windows[-1].destroy()
+
 
 if __name__ == "__main__":
     print("started")
+
     api = API()
-    print("window started")
+
     screen_w, screen_h = pyautogui.size()
+
     webview.create_window(
         "Aura-Ctrl",
         "ui/index.html",
-        js_api=api,
+        js_api=api,        # API attached to main window
         width=screen_w,
         height=screen_h
     )
+
     webview.start()
